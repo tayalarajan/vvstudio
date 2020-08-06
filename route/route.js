ceba11acee2ba9383630ff2f12814a3f7d1e95a5
tayalarajan <tayalarajan45@gmail.com>
2020-08-06 16:56:35 +0530

    spreadsheet included

--- route/route.js
+++ route/route.js
@@ -1,23 +1,40 @@
 var router = require('express').Router()
-var fire = require('./fire')
+const GoogleSpreadsheet = require('google-spreadsheet')
+const { promisify } = require('util')
 var bodyParser = require('body-parser')
-var db = fire.firestore()
 router.use(bodyParser.json())
 const checksum_lib = require('./checksum')
 var today = new Date()
 const port = 3000
+const qs = require('querystring');
+
+const creds = require('../client_secret.json')
+
+var PaytmConfig = {
+	mid: "eQplcx57912286320175",
+	key: "oiJiPd9q1o@PXzJC",
+	website: "DEFAULT"
+}
 
 
 router.post("/check", async function (req, res) {
-    await db.collection('appointment').add({
+    const doc = new GoogleSpreadsheet('1EHLd__MfnXtgpoS5AinHECxH6agjMN6yC8JNwv2JGTQ')
+    await promisify(doc.useServiceAccountAuth)(creds)
+
+    const info = await promisify(doc.getInfo)()
+    const sheet = info.worksheets[0]
+
+    const row = {
         name: req.body.name,
         email: req.body.email,
         mobilenumber: req.body.mobilenumber,
         service: req.body.service,
         payment: req.body.payment,
-        date: req.body.date,
-        timestamp: new Date(),
-    });
+        date: req.body.date
+    }
+
+    await promisify(sheet.addRow)(row)
+
     if(req.body.payment=="Pay later"){
         res.render("success.ejs");
     }
@@ -26,25 +43,21 @@ router.post("/check", async function (req, res) {
     }
 });
 
-router.get("/check", function (req, res) {
-    res.render("success.ejs");
-});
-
 router.get('/payment', (req, res)=>{
     let params ={}
-    params['MID'] = 'xQVIdU26063268309668',
-    params['WEBSITE'] = 'WEBSTAGING',
+    params['MID'] = PaytmConfig.mid,
+    params['WEBSITE'] = 'DEFAULT',
     params['CHANNEL_ID'] = 'WEB',
     params['INDUSTRY_TYPE_ID'] = 'Retail',
     params['ORDER_ID'] = 'ORD' + today.getDate() + today.getHours() + today.getMinutes( )+ today.getSeconds(),
     params['CUST_ID'] = 'CUST' + today.getDate() + today.getHours() + today.getMinutes( )+ today.getSeconds(),
-    params['TXN_AMOUNT'] = '100',
-    params['CALLBACK_URL'] = 'https://vvstudio.herokuapp.com/check',
+    params['TXN_AMOUNT'] = '1',
+    params['CALLBACK_URL'] = 'https://vvstudio.herokuapp.com/success',
     params['EMAIL'] = 'test@gmail.com',
     params['MOBILE_NO'] = '8489797055'
 
-    checksum_lib.genchecksum(params, 'smLH6u8xAWOZ49zp', function(err,checksum){
-        let txn_url = 'https://securegw-stage.paytm.in/order/process'
+    checksum_lib.genchecksum(params, PaytmConfig.key, function(err,checksum){
+        let txn_url = 'https://securegw.paytm.in/order/process'
         let form_fields = ""
         for(x in params)
         {
@@ -62,5 +75,10 @@ router.get('/payment', (req, res)=>{
         res.end()
     })    
 })
+
+router.post('/success', (req, res) => {
+    res.render('success.ejs')
+})
+
 
 module.exports = router
